7b962e9ae0a8b455c4d0749199646190f86550c5
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2021-01-08 21:08:22 -0800

    Commiting changes including successful invoice example

--- playingWithPytesseract/playingWithPytesseract.py
+++ playingWithPytesseract/playingWithPytesseract.py
@@ -1,13 +1,17 @@
 import pytesseract as tess
 from PIL import Image
 
-img = Image.open('help.png')
-img2= Image.open('arialFont.png')
+img = Image.open('help.png') #paint txt image
+img2= Image.open('arialFont.png') #testing arial font
+img3= Image.open('ocrImage.png') #invoice
+
 text = tess.image_to_string(img)
 text2 = tess.image_to_string(img2)
+text3 = tess.image_to_string(img3)
 
+#writing invoice image to file and printing results on console
 text_file = open("Output.txt", "w")
-text_file.write(text + text2)
+text_file.write(text3)
 text_file.close()
 text_file = open("Output.txt", "r")
 print(text_file.read())
